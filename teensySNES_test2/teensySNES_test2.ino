1802e8aec7729e681f2a748fca3c8c3f065c1ab5
driverblock <rick@embedded-designs.com>
2013-08-15 11:09:12 -0400

    Modified to support multiple keypresses
    Teensy Keyboard class supports up to 6 simultaneous keypresses. The
    previous teensySNES_test code did not take advantage of this, and only
    supported single keypresses. Most games require multiple keypresses.

--- teensySNES_test2/teensySNES_test2.ino
+++ teensySNES_test2/teensySNES_test2.ino
@@ -25,11 +25,18 @@ const int pinBtnTrigRight = 23;
 const int pinLEDOutput = 11;
 
 //Variables for the states of the SNES buttons
-byte buttons[] = {pinBtnUp, pinBtnRight, pinBtnDown, pinBtnLeft, pinBtnSelect, pinBtnStart,
-pinBtnB, pinBtnA, pinBtnY, pinBtnX, pinBtnTrigLeft, pinBtnTrigRight}; 
-byte keys[] = {KEY_U, KEY_R, KEY_D, KEY_L, KEY_ENTER, KEY_TAB, KEY_B, KEY_A, KEY_Y, KEY_X, KEY_P, KEY_Q};
+byte buttons[] = {  pinBtnUp, pinBtnRight, pinBtnDown, pinBtnLeft, pinBtnSelect, pinBtnStart,
+                    pinBtnB, pinBtnA, pinBtnY, pinBtnX, pinBtnTrigLeft, pinBtnTrigRight
+                 }; 
+short keys[] = {KEY_U, KEY_R, KEY_D, KEY_L, KEY_ENTER, KEY_TAB, KEY_B, KEY_A, KEY_Y, KEY_X, KEY_P, KEY_Q};
 
 #define NUMBUTTONS sizeof(buttons)
+
+typedef void KeyFunction_t(uint8_t c);
+
+KeyFunction_t* buttonActive[NUMBUTTONS];
+KeyFunction_t* keyList[] = {myset_key6, myset_key5, myset_key4, myset_key3, myset_key2, myset_key1};
+int            keySlot = sizeof(keyList) / sizeof(KeyFunction_t*);
 
 //Change these values if accelerometer reading are different:
 //How far the accerometer is tilted before
@@ -173,40 +180,79 @@ void fcnProcessAccelerometer()
 
 //Function to process the buttons from the SNES controller
 void fcnProcessButtons()
-{
-  static long currentkey = 0;
-  byte nothingpressed = 1;
+{ 
+  bool keysPressed = false; 
+  bool keysReleased = false;
   
   // run through all the buttons
   for (byte i = 0; i < NUMBUTTONS; i++) {
     
     // are any of them pressed?
-    if (! digitalRead(buttons[i])) {
-      nothingpressed = 0; // at least one button is pressed!
-      
-      // if its a new button, release the old one, and press the new one
-      if (currentkey != keys[i]) {
-        Keyboard.set_key1(0);
-        Keyboard.send_now();
-        Keyboard.set_key1(keys[i]);
-        currentkey = keys[i];
-        Keyboard.send_now();
-        delay(KEYDELAY);
-      } else {
-        // the same button is pressed, so repeat!
-        Keyboard.set_key1(keys[i]);
-        Keyboard.send_now();
-        delay(KEYREPEAT);
+    if (! digitalRead(buttons[i])) 
+    {                              //this button is pressed
+      keysPressed = true;
+      if (!buttonActive[i])        //was it pressed before?
+        activateButton(i);            //no - activate the keypress
+    }
+    else
+    {                              //this button is not pressed
+      if (buttonActive[i]) {        //was it pressed before?
+        releaseButton(i);            //yes - release the keypress
+        keysReleased = true;
       }
     }
   }
   
-  if (nothingpressed) {
-    // release all keys
-    Keyboard.set_key1(0);
-    Keyboard.send_now();
+  if (keysPressed || keysReleased)
+    Keyboard.send_now();            //update all the keypresses
+
+}
+
+void activateButton(byte index)
+{
+  if (keySlot)      //any key slots left?
+  {
+    keySlot--;                                //Push the keySlot stack
+    buttonActive[index] = keyList[keySlot];   //Associate the keySlot function pointer with the button
+    (*keyList[keySlot])(keys[index]);         //Call the key slot function to set the key value 
   }
 }
 
+void releaseButton(byte index)
+{
+  keyList[keySlot] = buttonActive[index];    //retrieve the keySlot function pointer
+  buttonActive[index] = 0;                   //mark the button as no longer pressed
+  (*keyList[keySlot])(0);                    //release the key slot
+  keySlot++;                                 //pop the keySlot stack
+}
 
+void myset_key1(uint8_t c)
+{
+  Keyboard.set_key1(c);
+}
+
+void myset_key2(uint8_t c)
+{
+  Keyboard.set_key2(c);
+}
+
+void myset_key3(uint8_t c)
+{
+  Keyboard.set_key3(c);
+}
+
+void myset_key4(uint8_t c)
+{
+  Keyboard.set_key4(c);
+}
+
+void myset_key5(uint8_t c)
+{
+  Keyboard.set_key5(c);
+}
+
+void myset_key6(uint8_t c)
+{
+  Keyboard.set_key6(c);
+}
 
